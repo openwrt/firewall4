openwrt/firewall4

commit 119ee1a06d4a5e5fd01ec1a242d21d6f355d7ff6

ruleset: drop ctstate invalid traffic for masq-enabled zones
For NAT enabled zones, stage rules to drop forwarded traffic with conntrack
state "invalid" and honor `masq_allow_invalid` option to inhibit those
rules.

This ports the corresponding firewall3 logic to firewall4.

Ref: https://forum.openwrt.org/t/x/140790
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

root/usr/share/firewall4/templates/ruleset.uc
@@ -265,6 +265,9 @@ table inet fw4 {
 {%   if (zone.dflags[verdict]): %}
 	chain {{ verdict }}_to_{{ zone.name }} {
 {%   for (let rule in zone.match_rules): %}
+{%     if (verdict == "accept" && (zone.masq || zone.masq6) && !zone.masq_allow_invalid): %}
+		{%+ include("zone-drop-invalid.uc", { fw4, zone, rule }) %}
+{%     endif %}
 		{%+ include("zone-verdict.uc", { fw4, zone, rule, egress: true, verdict }) %}
 {%   endfor %}
 	}

root/usr/share/firewall4/templates/zone-drop-invalid.uc
@@ -1,0 +1,8 @@
+{%+ if (zone.masq ^ zone.masq6): -%}
+	meta nfproto {{ fw4.nfproto(zone.masq ? 4 : 6) }} {%+ endif -%}
+{%+  include("zone-match.uc", { egress: true, rule }) -%}
+ct state invalid {%+ if (zone.counter): -%}
+	counter {%+ endif -%}
+{%+ if (zone.log & 1): -%}
+	log prefix "drop {{ zone.name }} invalid ct state: " {%+ endif -%}
+drop comment "!fw4: Prevent NAT leakage"

tests/01_configuration/01_ruleset
@@ -213,6 +213,7 @@ table inet fw4 {
 	}
 
 	chain accept_to_wan {
+		meta nfproto ipv4 oifname "pppoe-wan" ct state invalid counter drop comment "!fw4: Prevent NAT leakage"
 		oifname "pppoe-wan" counter accept comment "!fw4: accept wan IPv4/IPv6 traffic"
 	}
 

tests/02_zones/02_masq
@@ -151,6 +151,7 @@ table inet fw4 {
 	}
 
 	chain accept_to_test1 {
+		meta nfproto ipv4 oifname "zone1" ct state invalid counter drop comment "!fw4: Prevent NAT leakage"
 		oifname "zone1" counter accept comment "!fw4: accept test1 IPv4/IPv6 traffic"
 	}
 

tests/02_zones/04_masq_allow_invalid
@@ -1,4 +1,4 @@
-Testing that zone masquerading restrictions source and destination restrictions are properly applied.
+Testing that dropping of invalid conntrack state traffic can be inhibited.
 
 -- Testcase --
 {%
@@ -29,62 +29,14 @@ Testing that zone masquerading restrictions source and destination restrictions 
 {
 	"zone": [
 		{
-			".description": "Positive and negative entries should be handled properly and IPv6 addresses should be filtered out for IPv4 masquerading",
+			".description": "No ct state invalid drop rule should be generated",
 			"name": "test1",
 			"input": "ACCEPT",
 			"output": "ACCEPT",
 			"forward": "ACCEPT",
 			"device": "zone1",
 			"masq": "1",
-			"masq_src": [
-				"10.1.0.0/24",
-				"10.1.1.1",
-				"!10.1.0.1",
-				"!10.1.0.2",
-				"2001:db8:0:1::/64",
-				"2001:db8:0:2::/64",
-				"!2001:db8:0:1::1",
-				"!2001:db8:0:1::2"
-			],
-			"masq_dest": [
-				"10.2.0.0/24",
-				"10.2.1.1",
-				"!10.2.0.1",
-				"!10.2.0.2",
-				"2001:db8:1:1::/64",
-				"2001:db8:1:2::/64",
-				"!2001:db8:1:1::1",
-				"!2001:db8:1:1::2"
-			]
-		},
-		{
-			".description": "Positive and negative entries should be handled properly and IPv4 addresses should be filtered out for IPv6 masquerading",
-			"name": "test2",
-			"input": "DROP",
-			"output": "DROP",
-			"forward": "DROP",
-			"device": "zone2",
-			"masq6": "1",
-			"masq_src": [
-				"10.1.0.0/24",
-				"10.1.1.1",
-				"!10.1.0.1",
-				"!10.1.0.2",
-				"2001:db8:0:1::/64",
-				"2001:db8:0:2::/64",
-				"!2001:db8:0:1::1",
-				"!2001:db8:0:1::2"
-			],
-			"masq_dest": [
-				"10.2.0.0/24",
-				"10.2.1.1",
-				"!10.2.0.1",
-				"!10.2.0.2",
-				"2001:db8:1:1::/64",
-				"2001:db8:1:2::/64",
-				"!2001:db8:1:1::1",
-				"!2001:db8:1:1::2"
-			]
+			"masq_allow_invalid": 1
 		}
 	]
 }
@@ -101,9 +53,6 @@ table inet fw4 {
 
 	define test1_devices = { "zone1" }
 	define test1_subnets = {  }
-
-	define test2_devices = { "zone2" }
-	define test2_subnets = {  }
 
 
 	#
@@ -124,7 +73,6 @@ table inet fw4 {
 
 		ct state established,related accept comment "!fw4: Allow inbound established and related flows"
 		iifname "zone1" jump input_test1 comment "!fw4: Handle test1 IPv4/IPv6 input traffic"
-		iifname "zone2" jump input_test2 comment "!fw4: Handle test2 IPv4/IPv6 input traffic"
 	}
 
 	chain forward {
@@ -132,7 +80,6 @@ table inet fw4 {
 
 		ct state established,related accept comment "!fw4: Allow forwarded established and related flows"
 		iifname "zone1" jump forward_test1 comment "!fw4: Handle test1 IPv4/IPv6 forward traffic"
-		iifname "zone2" jump forward_test2 comment "!fw4: Handle test2 IPv4/IPv6 forward traffic"
 	}
 
 	chain output {
@@ -142,7 +89,6 @@ table inet fw4 {
 
 		ct state established,related accept comment "!fw4: Allow outbound established and related flows"
 		oifname "zone1" jump output_test1 comment "!fw4: Handle test1 IPv4/IPv6 output traffic"
-		oifname "zone2" jump output_test2 comment "!fw4: Handle test2 IPv4/IPv6 output traffic"
 	}
 
 	chain prerouting {
@@ -171,28 +117,7 @@ table inet fw4 {
 	}
 
 	chain accept_to_test1 {
-		meta nfproto ipv4 oifname "zone1" ct state invalid counter drop comment "!fw4: Prevent NAT leakage"
 		oifname "zone1" counter accept comment "!fw4: accept test1 IPv4/IPv6 traffic"
-	}
-
-	chain input_test2 {
-		jump drop_from_test2
-	}
-
-	chain output_test2 {
-		jump drop_to_test2
-	}
-
-	chain forward_test2 {
-		jump drop_to_test2
-	}
-
-	chain drop_from_test2 {
-		iifname "zone2" counter drop comment "!fw4: drop test2 IPv4/IPv6 traffic"
-	}
-
-	chain drop_to_test2 {
-		oifname "zone2" counter drop comment "!fw4: drop test2 IPv4/IPv6 traffic"
 	}
 
 
@@ -207,15 +132,10 @@ table inet fw4 {
 	chain srcnat {
 		type nat hook postrouting priority srcnat; policy accept;
 		oifname "zone1" jump srcnat_test1 comment "!fw4: Handle test1 IPv4/IPv6 srcnat traffic"
-		oifname "zone2" jump srcnat_test2 comment "!fw4: Handle test2 IPv4/IPv6 srcnat traffic"
 	}
 
 	chain srcnat_test1 {
-		meta nfproto ipv4 ip saddr { 10.1.0.0/24, 10.1.1.1 } ip saddr != { 10.1.0.1, 10.1.0.2 } ip daddr { 10.2.0.0/24, 10.2.1.1 } ip daddr != { 10.2.0.1, 10.2.0.2 } masquerade comment "!fw4: Masquerade IPv4 test1 traffic"
-	}
-
-	chain srcnat_test2 {
-		meta nfproto ipv6 ip6 saddr { 2001:db8:0:1::/64, 2001:db8:0:2::/64 } ip6 saddr != { 2001:db8:0:1::1, 2001:db8:0:1::2 } ip6 daddr { 2001:db8:1:1::/64, 2001:db8:1:2::/64 } ip6 daddr != { 2001:db8:1:1::1, 2001:db8:1:1::2 } masquerade comment "!fw4: Masquerade IPv6 test2 traffic"
+		meta nfproto ipv4 masquerade comment "!fw4: Masquerade IPv4 test1 traffic"
 	}
 
 

commit 7392792e768b4d5c373f66ec400fd4100df1b4e0

ruleset: do not emit redundant drop invalid rules
The wan interface drop rule unnecessarily persists when invalid state
is dropped globally and the rule cannot catch anything at all, so remove
it as the effect is achieved by default and to global extent.

Fixes: 119ee1a ("ruleset: drop ctstate invalid traffic for masq-enabled zones")
Signed-off-by: Andris PE <neandris@gmail.com>
[fix S-o-b tag, fix commit author, reword commit subject and message]
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

root/usr/share/firewall4/templates/ruleset.uc
@@ -276,7 +276,7 @@ table inet fw4 {
 {%   if (zone.dflags[verdict]): %}
 	chain {{ verdict }}_to_{{ zone.name }} {
 {%   for (let rule in zone.match_rules): %}
-{%     if (verdict == "accept" && (zone.masq || zone.masq6) && !zone.masq_allow_invalid): %}
+{%     if (!fw4.default_option("drop_invalid") && verdict == "accept" && (zone.masq || zone.masq6) && !zone.masq_allow_invalid): %}
 		{%+ include("zone-drop-invalid.uc", { fw4, zone, rule }) %}
 {%     endif %}
 		{%+ include("zone-verdict.uc", { fw4, zone, rule, egress: true, verdict }) %}