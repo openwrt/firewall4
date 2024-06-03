openwrt/firewall4

commit e00958884416f59b273595f941d198de63acc1dd

fw4: do not add physical devices for soft offload
Let kernel heuristics take care of offloading decapsulation.

When software flow offloading is requested, avoid manually resolving and
adding lower physical devices to the flow table in order to let kernel
heuristics deal with the proper offloading en/decapsulation.

Fixes: https://github.com/openwrt/openwrt/issues/13410
Ref: https://github.com/openwrt/openwrt/issues/10224
Submitted-by: Andris PE <neandris@gmail.com>
[refactor code, reword commit message]
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

root/usr/share/ucode/fw4.uc
@@ -505,12 +505,12 @@ return {
 		return v;
 	},
 
-	resolve_offload_devices: function() {
-		if (!this.default_option("flow_offloading"))
-			return [];
+	resolve_hw_offload_devices: function() {
+		if (!this.default_option("flow_offloading_hw"))
+			return null;
 
 		let devstatus = null;
-		let devices = [];
+		let devices = null;
 		let bus = ubus.connect();
 
 		if (bus) {
@@ -520,15 +520,36 @@ return {
 
 		for (let zone in this.zones())
 			for (let device in zone.related_physdevs)
-				push(devices, ...resolve_lower_devices(devstatus, device));
+				push(devices ||= [], ...resolve_lower_devices(devstatus, device));
+
+		if (!devices)
+			return null;
+
 		devices = sort(uniq(devices));
 
-		if (this.default_option("flow_offloading_hw")) {
-			if (length(devices) && nft_try_hw_offload(devices))
-				return devices;
-
+		if (!nft_try_hw_offload(devices)) {
 			this.warn('Hardware flow offloading unavailable, falling back to software offloading');
 			this.state.defaults.flow_offloading_hw = false;
+
+			return null;
+		}
+
+		return devices;
+	},
+
+	resolve_offload_devices: function() {
+		if (!this.default_option("flow_offloading"))
+			return [];
+
+		let devices = this.resolve_hw_offload_devices();
+
+		if (!devices) {
+			devices = [];
+
+			for (let zone in this.zones())
+				push(devices, ...zone.related_physdevs);
+
+			devices = sort(uniq(devices));
 		}
 
 		return devices;

commit dfbcc1cd127c78fc61bb870d36d2512b571d223b

fw4: skip not existing netdev names in flowtable device list
In case interface configurations are present which refer to not existing
network devices, such device names might end up in the flowtable list,
leading to `No such file or directory` errors when attempting to load
the resulting ruleset.

Solve this issue by testing for each netdev name whether it refers to
an existing device.

Fixes: e009588 ("fw4: do not add physical devices for soft offload")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

root/usr/share/ucode/fw4.uc
@@ -547,7 +547,9 @@ return {
 			devices = [];
 
 			for (let zone in this.zones())
-				push(devices, ...zone.related_physdevs);
+				for (let device in zone.related_physdevs)
+					if (fs.access(`/sys/class/net/${device}`))
+						push(devices, device);
 
 			devices = sort(uniq(devices));
 		}